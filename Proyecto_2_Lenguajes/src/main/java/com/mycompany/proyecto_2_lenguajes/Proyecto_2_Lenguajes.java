13375dc400c3349b4ce31bae6c2052d335d05148
HersonAJ <hersonaguilar202230685@cunoc.edu.gt>
2025-04-26 12:22:10 -0600

    iniciando el proyecto

---
+++ Proyecto_2_Lenguajes/src/main/java/com/mycompany/proyecto_2_lenguajes/Proyecto_2_Lenguajes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_2_lenguajes;
+
+/**
+ *
+ * @author herson
+ */
+public class Proyecto_2_Lenguajes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
